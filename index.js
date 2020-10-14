58d835d4c62ad7c96c9bc686ef947ef2d8729783
Luong Trung Thanh <thnhlngtrung@gmail.com>
2020-10-14 17:46:53 +0700

    add module

--- index.js
+++ index.js
@@ -1,67 +1,69 @@
 'use strict';
 
+const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 const server = express();
-const request = require('request');
 server.use(bodyParser.json());
-const {
-  dialogflow,
-  Suggestions,
-  Permission, 
-  SimpleResponse
-} = require('actions-on-google');
-const NodeGeocoder = require("node-geocoder");
-const app = dialogflow({debug: true});
+// const {
+//   dialogflow,
+//   Suggestions,
+//   Permission, 
+//   SimpleResponse
+// } = require('actions-on-google');
+// const NodeGeocoder = require("node-geocoder");
+// const app = dialogflow({debug: true});
 
 // app.intent('Default Fallback Intent', (conv) => {
 //     conv.ask('Please repeat');
 // });
 
-app.intent("Default Welcome Intent", conv => {
-  conv.data.requestedPermission = "DEVICE_PRECISE_LOCATION";
-  conv.ask(new SimpleResponse('Welcome to location tracker'));
-  return conv.ask(
-    new Permission({
-      context: "to locate you",
-      permissions: conv.data.requestedPermission
-    })
-  );
-});
+// app.intent("Default Welcome Intent", conv => {
+//   conv.data.requestedPermission = "DEVICE_PRECISE_LOCATION";
+//   conv.ask(new SimpleResponse('Welcome to location tracker'));
+//   return conv.ask(
+//     new Permission({
+//       context: "to locate you",
+//       permissions: conv.data.requestedPermission
+//     })
+//   );
+// });
 
-app.intent("get_current_location", (conv, params, permissionGranted) => {
-  if (permissionGranted) {
-    const { requestedPermission } = conv.data;
-    //let address;
-    if (requestedPermission === "DEVICE_PRECISE_LOCATION") {
-      const { coordinates } = conv.device.location;
-      console.log('coordinates are', coordinates);
+// app.intent("get_current_location", (conv, params, permissionGranted) => {
+//   if (permissionGranted) {
+//     const { requestedPermission } = conv.data;
+//     //let address;
+//     if (requestedPermission === "DEVICE_PRECISE_LOCATION") {
+//       const { coordinates } = conv.device.location;
+//       console.log('coordinates are', coordinates);
 
-      //if (coordinates && address) {
-      if (coordinates) {
+//       //if (coordinates && address) {
+//       if (coordinates) {
 
-var url = "https://nominatim.openstreetmap.org/reverse?lat="+coordinates.latitude+"&lon="+coordinates.longitude+"&format=json";
+// var url = "https://nominatim.openstreetmap.org/reverse?lat="+coordinates.latitude+"&lon="+coordinates.longitude+"&format=json";
 
-request("http://vntalking.com",function(error,response,body){
-    console.log(body);
-    return conv.close(new SimpleResponse(`Your Location details ${coordinates.latitude}, ${coordinates.longitude}, ${url}`));
-});
+// request("http://vntalking.com",function(error,response,body){
+//     console.log(body);
+//     return conv.close(new SimpleResponse(`Your Location details ${coordinates.latitude}, ${coordinates.longitude}, ${url}`));
+// });
 
 
        
 
      // return conv.close(new SimpleResponse(`Your Location details ${coordinates.latitude}, ${coordinates.longitude}, ${url}`));
-      } else {
+
+      //} else {
         // Note: Currently, precise locaton only returns lat/lng coordinates on phones and lat/lng coordinates
         // and a geocoded address on voice-activated speakers.
         // Coarse location only works on voice-activated speakers.
-        return conv.close("Sorry, I could not figure out where you are.");
-      }
-    }
-  } else {
-    return conv.close("Sorry, permission denied.");
-  }
-});
+
+//         return conv.close("Sorry, I could not figure out where you are.");
+//       }
+//     }
+//   } else {
+//     return conv.close("Sorry, permission denied.");
+//   }
+// });
 
 // app.intent('Default Welcome Intent', (conv) => {
 //     conv.ask('Hi, what do you wanna talk about?');
@@ -71,7 +73,7 @@ request("http://vntalking.com",function(error,response,body){
 // handlers for other intents..
 
 
-server.post('/hook', app);
+//server.post('/hook', app);
 
 
 server.get("/url", function(req, res) {
