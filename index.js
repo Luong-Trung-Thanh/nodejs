d28c057820dc1ad2da324f54b759f8b3ca5a1095
Luong Trung Thanh <thnhlngtrung@gmail.com>
2020-10-14 08:50:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,48 @@
 
 const {
   dialogflow,
-  Suggestions
+  Suggestions,
+  Permission, 
+  SimpleResponse
 } = require('actions-on-google');
+const NodeGeocoder = require("node-geocoder");
 const app = dialogflow({debug: true});
 
-app.intent('Default Fallback Intent', (conv) => {
-    conv.ask('Please repeat');
+// app.intent('Default Fallback Intent', (conv) => {
+//     conv.ask('Please repeat');
+// });
+
+app.intent("Default Welcome Intent", conv => {
+  conv.data.requestedPermission = "DEVICE_PRECISE_LOCATION";
+  conv.ask(new SimpleResponse('Welcome to location tracker'));
+  return conv.ask(
+    new Permission({
+      context: "to locate you",
+      permissions: conv.data.requestedPermission
+    })
+  );
+});
+
+app.intent("get_current_location", (conv, params, permissionGranted) => {
+  if (permissionGranted) {
+    const { requestedPermission } = conv.data;
+    let address;
+    if (requestedPermission === "DEVICE_PRECISE_LOCATION") {
+      const { coordinates } = conv.device.location;
+      console.log('coordinates are', coordinates);
+
+      if (coordinates && address) {
+        return conv.close(new SimpleResponse(`Your Location details ${address}`));
+      } else {
+        // Note: Currently, precise locaton only returns lat/lng coordinates on phones and lat/lng coordinates
+        // and a geocoded address on voice-activated speakers.
+        // Coarse location only works on voice-activated speakers.
+        return conv.close("Sorry, I could not figure out where you are.");
+      }
+    }
+  } else {
+    return conv.close("Sorry, permission denied.");
+  }
 });
 
 app.intent('Default Welcome Intent', (conv) => {
